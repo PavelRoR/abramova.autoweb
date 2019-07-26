f6b13f56bec92229dfb47b22c60f926e2c82e345
pavelp21 <pavelvp2182@yandex.ru>
2019-07-26 14:53:09 +0500

    Author section is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,8 +1,27 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-
-/*Конец документа*/
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
+    });
+    /*Конец документа*/
 });
